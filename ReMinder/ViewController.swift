b614c4d8d45906c10794921e206994fe6774e3bc
Chao Ming Huang <mingqed@gmail.com>
2025-02-17 12:22:51 +0800

    Initial Commit

---
+++ ReMinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReMinder
+//
+//  Created by 黃昭銘 on 2025/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
